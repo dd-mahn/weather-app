97eb9a464cd145170a82c9a010d1deffc0c5fa87
dd-mahn <ducmanh0801@gmail.com>
2024-04-16 14:33:02 +0700

    initial

--- webpack.config.js
+++ webpack.config.js
@@ -15,7 +15,7 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: "./public/index.html",
-    //   favicon: "./public/favicon.ico",
+      favicon: "./public/favicon.ico",
       filename: "index.html",
       inject: "body",
     }),
